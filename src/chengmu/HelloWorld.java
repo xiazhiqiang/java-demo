b95b2b88ae1c16635d5646e4ec8f28dbea46e6e6
xiazhiqiang <student_good@126.com>
2020-12-02 13:27:25 +0800

    调试URL解析

--- src/chengmu/HelloWorld.java
+++ src/chengmu/HelloWorld.java
@@ -1,9 +1,16 @@
 package chengmu;
 
 import java.lang.System.*;
+import java.net.*;
+// import android.net.Uri;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    out.println("Hello world!");
+    System.out.println("Hello world!");
+    // String url = "https://portal.zjzwfw.gov.cn/?DeviceID=11704301";
+    String url = "https://portal.zjzwfw.gov.cn/ ?DeviceID=11704301";
+    // String url = "https://portal.zjzwfw.gov.cn/%20?DeviceID=11704301";
+    URI Uri = URI.create(url);
+    System.out.println("params: " + Uri.getQuery().toString());
   }
 }
